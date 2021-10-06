c50cb0540dd6af7660988d437fe9179806f31ad6
Poonnatuch Boonyarattanasoontorn <6110613228@student.tu.ac.th>
2021-10-06 16:04:25 +0700

    Initial IncubatorTable components

---
+++ src/views/IncubatorTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <IncubatorTable />
+</template>
+
+<script>
+import IncubatorTable from "../components/IncubatorTable.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    IncubatorTable,
+  },
+};
+</script>
+
+<style></style>
