6f3ae26ee61abd030f44e4dc3c3bc0c768d71cb2
yodsatorn pantongkam <yodsatornb@gmail.com>
2021-10-09 00:06:43 +0700

    about page and nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <v-app>
-    <ul>
-      <li>
-        <router-link to="/"> Home </router-link>
-      </li>
-      <li>
-        <router-link to="/about"> About </router-link>
-      </li>
-    </ul>
+    <v-app-bar color="#1C2833" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>CN466 Midterm Project</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="#2471A3--text text--accent-4" 
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <router-link to="/">
+              <v-list-item-title>Home</v-list-item-title>
+            </router-link>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-information</v-icon>
+            </v-list-item-icon>
+            <router-link to="/about">
+              <v-list-item-title>About us</v-list-item-title>
+            </router-link>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
 
 <style scoped>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container>
-    <h1>About page</h1>
+    <h1>About us</h1>
     <v-card class="mx-auto" style="max-width:100% ;">
       <v-img
         src="https://images.pexels.com/photos/3183190/pexels-photo-3183190.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        height="300px"
+        height="400px"
       ></v-img>
       <v-card-title class="text-h5">
         CN466 - Midterm Project
       </v-card-title>
       <v-card-subtitle class="text-body-1">
-        This project is about ....
+        This project is built as part of the CN466 midterm project, the front-end is provided by Yodsatorn Pantongkam using Vue.js as the framework front-end for development. 
       </v-card-subtitle>
       <v-card-actions>
         <v-btn color="orange lighten-2" text>
@@ -24,10 +24,10 @@
       <v-expand-transition>
         <div v-show="show">
           <v-divider></v-divider>
-          <v-card-title >
+          <v-card-title>
             Member
           </v-card-title>
-          <v-card-subtitle >
+          <v-card-subtitle>
             - 6110613095 : Yodsatorn Pantongkam <br />
             - 6110613129 : Rathapol Putharaksa <br />
             - 6110613228 : Poonnatuch Boonyarattanasoontorn <br />
@@ -36,13 +36,28 @@
             <v-icon large>
               mdi-github
             </v-icon>
-            <br>  <br>
+            <v-text> Back-end APIs : </v-text>
+            <a href="https://www.instagram.com/poonnatuchh/"
+              >Link to Git repository</a
+            >
+            <br />
+            <br />
             <v-icon large>
               mdi-github
             </v-icon>
+            <v-text> Front-end : </v-text>
+            <a href="https://github.com/yodsatorn/cn466-midterm-frontend.git"
+              >Link to Git repository</a
+            >
+            <br />
+            <br />
             <v-icon large>
               mdi-github
             </v-icon>
+            <v-text> Line Chatbot : </v-text>
+            <a href="https://github.com/Rathapol-Putharaksa/linebot_CN466"
+              >Link to Git repository</a
+            >
           </v-col>
         </div>
       </v-expand-transition>
